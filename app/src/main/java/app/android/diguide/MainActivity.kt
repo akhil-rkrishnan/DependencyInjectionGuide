6c476cbfddbada9ec88fce8e3ced23f62f8c558c
Akhil krishnan <akhil.krishnan@accubits.com>
2022-11-10 11:40:30 +0530

    first commit

---
+++ app/src/main/java/app/android/diguide/MainActivity.kt
@@ -0,0 +1,43 @@
+package app.android.diguide
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.android.diguide.ui.theme.DependencyInjectionGuideTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DependencyInjectionGuideTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DependencyInjectionGuideTheme {
+        Greeting("Android")
+    }
+}
