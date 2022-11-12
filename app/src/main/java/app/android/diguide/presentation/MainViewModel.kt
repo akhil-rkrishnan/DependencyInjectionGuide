488a68964fcb5d04848b2c84dd8b4e4994bd8dc4
Akhil krishnan <akhil.krishnan@accubits.com>
2022-11-12 15:25:39 +0530

    Qualifiers for Koin

--- app/src/main/java/app/android/diguide/presentation/MainViewModel.kt
+++ app/src/main/java/app/android/diguide/presentation/MainViewModel.kt
@@ -2,7 +2,6 @@ package app.android.diguide.presentation
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.android.diguide.model.ApiModel
@@ -13,6 +12,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 private const val TAG = "MainViewModel"
+
 class MainViewModel(
     private val mainRepository: MainRepository
 ) : ViewModel() {
@@ -28,13 +28,14 @@ class MainViewModel(
             try {
                 mainRepository.getData().enqueue(object : Callback<ApiModel> {
                     override fun onResponse(call: Call<ApiModel>, response: Response<ApiModel>) {
-                        Log.e(TAG, "onResponse: ${response.body()}", )
+                        Log.e(TAG, "onResponse: ${response.body()}")
                         if (response.isSuccessful) {
                             response.body()?.let {
                                 _apiData.value = it.entries
                             }
                         }
                     }
+
                     override fun onFailure(call: Call<ApiModel>, t: Throwable) {
 
                     }
